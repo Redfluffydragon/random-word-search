3160b0a0ee6efd765dd2b2a76a8cd36b60476f8a
Redfluffydragon <kderu1420@gmail.com>
2021-12-28 17:36:23 -0700

    Better variable names and formatting, and only highlight on left click, and get rid of unecessary global variables

--- script.js
+++ script.js
@@ -5,10 +5,7 @@
 
 // localStorage.clear();
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let tweentable = []; //push to here, then make into one string later
-let table = [];//for letters
 let cells = [];//for cells
-let cellinfo = []; //highlighted or not
 let width = 45; //45 max width for HP
 let height = 19;
 
@@ -21,7 +18,7 @@ const ldmode = document.getElementById('ldmode');
 
 const wordsearch = document.getElementById('wordsearch');
 const newtable = document.getElementById('newtable');
-const savetable = document.getElementById('savetable');
+const saveTable = document.getElementById('savetable');
 const saved = document.getElementById('saved');
 const deleteall = document.getElementById('deleteall');
 const savedpopup = document.getElementById('savedpopup');
@@ -32,7 +29,7 @@ const savepopup = document.getElementById('savepopup');
 const givename = document.getElementById('givename');
 const reallysave = document.getElementById('reallysave');
 
-const saveAlert = document.getElementById('saveAlert'); 
+const saveAlert = document.getElementById('saveAlert');
 
 const backbtn = document.getElementById('backbtn');
 const savedName = document.getElementById('savedName');
@@ -49,44 +46,46 @@ function drawtable(table, index) {
   cells.length = 0;
   for (let i = 0; i < /* alltables[index]. */height; i++) {
     let row = table.insertRow(0);
-    for(let j = 0; j < /* alltables[index]. */width; j++) {
+    for (let j = 0; j < /* alltables[index]. */width; j++) {
       let cell = row.insertCell(0);
       cells.push(cell);
     }
   }
 }
 
-function filltable() {
-  tweentable.length = 0;
-  for (let i = 0; i < width*height; i++) {
-    let randomLetter = Math.round(Math.random()*25);
-    tweentable.push(letters[randomLetter]);
-    cellinfo.push(0);
+/** Fill a table with random letters and push it to allTables */
+function fillTable() {
+  const tweenTable = [];
+  const cellInfo = [];
+
+  for (let i = 0; i < width * height; i++) {
+    tweenTable.push(letters[Math.floor(Math.random() * letters.length)]);
+    cellInfo.push(0);
   }
-  table = tweentable.join('');
-  for (let i = 0; i < width*height; i++) {
+  const table = tweenTable.join('');
+  for (let i = 0; i < width * height; i++) {
     cells[i].textContent = table.charAt(i);
   }
-  alltables.push({'name': '', 'number': alltables.length+1, 'table': table, 'highlit': cellinfo, width: width, height: height});
-  localStorage.setItem('saved', JSON.stringify(alltables));
+  allTables.push({ 'name': '', 'number': allTables.length + 1, 'table': table, 'highlit': cellInfo, width: width, height: height });
+  localStorage.setItem('savedTables', JSON.stringify(allTables));
 }
 
 function ifSaved() { //If viewing saved table
   backbtn.classList.remove('none'); //show the back button
   newtable.classList.add('none'); //hide the new table button
   document.title = 'RWS - ' + tableName; //change the title
-  savedName.value = alltables[shown].name; //put the name of the saved table
+  savedName.value = allTables[shown].name; //put the name of the saved table
   seeName.classList.remove('none'); //show the name
-  savetable.classList.add('none'); //hide the save button
+  saveTable.classList.add('none'); //hide the save button
 }
 
 function changeName() { //change the name of a saved table
   if (shown !== null) {
     let getName = savedName.value;
     if (givename.value !== '') {
-      alltables[shown].name = getName;
+      allTables[shown].name = getName;
     }
-    localStorage.setItem('all', JSON.stringify(alltables));
+    localStorage.setItem('all', JSON.stringify(allTables));
     saveAlert.classList.add('inlineBlock');
     shadow.style.display = 'initial';
     window.setTimeout(() => {
@@ -96,194 +95,223 @@ function changeName() { //change the name of a saved table
   }
 }
 
-function gotchem(item, defalt, type=localStorage) {
+function gotchem(item, defalt, type = localStorage) {
   let getem = type.getItem(item);
   if (getem !== null && JSON.parse(getem) !== undefined) return JSON.parse(getem);
-  return defalt; 
+  return defalt;
 }
-let alltables = gotchem('saved', []); //all the saved tables
+let allTables = gotchem('savedTables', []); //all the saved tables
 let shown = gotchem('shown', null, sessionStorage); //showing a saved table or not
 let tableName = gotchem('tableName', null, sessionStorage); //table name for shown
 let savedyet = gotchem('savedyet', false); //newest table saved yet or not
 let lightmode = gotchem('mode', true); //light or dark mode
 switchMode(true);
 
-function draw(newTable=false, idx=alltables.length-1) {
-  if (shown < alltables.length-1 && shown !== null) {
+function draw(newTable = false, idx = allTables.length - 1) {
+  if (shown < allTables.length - 1 && shown !== null) {
     ifSaved();
     idx = shown;
   }
   wordsearch.innerHTML = '';
   savedTables.innerHTML = '';
 
-  alltables.forEach((e, i) => {
-    e.number = i+1;
-  })
+  allTables.forEach((e, i) => {
+    e.number = i + 1;
+  });
   drawtable(wordsearch, idx);
 
-  if (alltables.length !== 0 && !newTable) {
+  if (allTables.length !== 0 && !newTable) {
     let getwidth = width; //attempt to use for different size tables
     let getheight = height;
-    
-    for (let i = 0; i < getwidth*getheight; i++) {
-      cells[i].textContent = alltables[idx].table[i];
-      if (alltables[idx].highlit[i]) {
-        cells[width*height-i-1].style.backgroundColor = alltables[idx].highlit[i];
+
+    for (let i = 0; i < getwidth * getheight; i++) {
+      cells[i].textContent = allTables[idx].table[i];
+      if (allTables[idx].highlit[i]) {
+        cells[width * height - i - 1].style.backgroundColor = allTables[idx].highlit[i];
       }
     }
   }
-  else { filltable(); idx = alltables.length-1; }
+  else {
+    fillTable();
+    idx = allTables.length - 1;
+  }
 
   cTable = idx; //for highlighting purposes
 
   if (savedyet) {
-    savetable.textContent = 'Saved!';
-    savetable.disabled = true;
+    saveTable.textContent = 'Saved!';
+    saveTable.disabled = true;
   }
   else {
-    savetable.textContent = 'Save';
-    savetable.disabled = false;
+    saveTable.textContent = 'Save';
+    saveTable.disabled = false;
   }
-  console.log(alltables);
+  console.log(allTables);
 }
 
 draw();
 
 //clicks on each letter - now with colors!
-let cellnum, highlighting, getcolor;
-let cellID = wordsearch.getElementsByTagName('td');
+let cellnum, highlighting, newHighlightColor;
+const cellID = wordsearch.getElementsByTagName('td');
+
+/**
+ * Generate a random RGB color
+ * @returns {Array} An array with three random integers from 0-255 (inclusive)
+ */
+function randomColor() {
+  const newColor = [];
+  for (let i = 0; i < 3; i++) {
+    newColor.push(Math.floor(Math.random() * 256));
+  }
+  return newColor;
+}
+
 function startDrag(e) {
   e.preventDefault();
-  let cellnum;
-  let whichmove;
+  let cellNum; // The index of the starting cell
+  let whichMove; // For adding the right event listener
   if (e.type === 'mousedown') {
-    cellnum = e.target.cellIndex + (e.target.closest('tr').rowIndex*width);
-    whichmove = 'mousemove';
+    if (e.button !== 0 || !e.target.matches('td')) { // Only color on left click, and only if you start on a cell (not on a row)
+      return;
+    }
+    cellNum = e.target.cellIndex + (e.target.closest('tr').rowIndex * width);
+    whichMove = 'mousemove';
   }
   else if (e.type === 'touchstart') {
-    let touchcell = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY)
-    cellnum = touchcell.cellIndex + (touchcell.closest('tr').rowIndex*width);
-    whichmove = 'touchmove';
+    const touchCell = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY);
+    if (!touchCell.matches('td')) { // Only color if you start on a cell
+      return;
+    }
+
+    cellNum = touchCell.cellIndex + (touchCell.closest('tr').rowIndex * width);
+    whichMove = 'touchmove';
   }
-  let makolor = [];
-  makolor.length = 0;
-  for (let i = 0; i < 3; i++) {
-    let randomcolor = Math.round(Math.random()*255);
-    makolor.push(randomcolor);
+
+  newHighlightColor = `rgba(${randomColor().join()}, 0.5)`;
+
+  if (allTables[cTable].highlit[cellNum] !== newHighlightColor) {
+    highlighting = true;
   }
-  getcolor = 'rgba(' + makolor.join() + ', 0.5)';
-  if (alltables[cTable].highlit[cellnum] !== getcolor) {highlighting = true;}
-  if (alltables[cTable].highlit[cellnum]) {highlighting = false};
-  if (!alltables[cTable].highlit[cellnum]) {
-    alltables[cTable].highlit[cellnum] = getcolor;
-    cellID[cellnum].style.backgroundColor = getcolor;
+  if (allTables[cTable].highlit[cellNum]) {
+    highlighting = false
   }
-  else if (alltables[cTable].highlit[cellnum]) {
-    alltables[cTable].highlit[cellnum] = false;
-    cellID[cellnum].style.backgroundColor = 'transparent';
+  if (!allTables[cTable].highlit[cellNum]) {
+    allTables[cTable].highlit[cellNum] = newHighlightColor;
+    cellID[cellNum].style.backgroundColor = newHighlightColor;
   }
-  wordsearch.addEventListener(whichmove, whileDragging, false);
+  else if (allTables[cTable].highlit[cellNum]) {
+    allTables[cTable].highlit[cellNum] = false;
+    cellID[cellNum].style.backgroundColor = 'transparent';
+  }
+
+  wordsearch.addEventListener(whichMove, whileDragging, false);
 }
+
 function whileDragging(e, type) { //added while dragging, removed when not
   if (e.target.closest('td')) {
     e.preventDefault();
     let cellnum;
     if (e.type === 'mousemove') {
-      cellnum = e.target.cellIndex + (e.target.closest('tr').rowIndex*width);
+      cellnum = e.target.cellIndex + (e.target.closest('tr').rowIndex * width);
     }
     else if (e.type === 'touchmove') {
       let touchcell = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY)
-      cellnum = touchcell.cellIndex + (touchcell.closest('tr').rowIndex*width);
+      cellnum = touchcell.cellIndex + (touchcell.closest('tr').rowIndex * width);
     }
-    if (alltables[cTable].highlit[cellnum] !== getcolor && highlighting) {
-      alltables[cTable].highlit[cellnum] = getcolor;
-      cellID[cellnum].style.backgroundColor = getcolor;
+    if (allTables[cTable].highlit[cellnum] !== newHighlightColor && highlighting) {
+      allTables[cTable].highlit[cellnum] = newHighlightColor;
+      cellID[cellnum].style.backgroundColor = newHighlightColor;
     }
-    else if (alltables[cTable].highlit[cellnum] !== getcolor && !highlighting) {
-      alltables[cTable].highlit[cellnum] = false;
+    else if (allTables[cTable].highlit[cellnum] !== newHighlightColor && !highlighting) {
+      allTables[cTable].highlit[cellnum] = false;
       cellID[cellnum].style.backgroundColor = 'transparent';
     }
   }
 }
+
 function endDrag(e) {
   let whichmove = e.type === 'mouseup' ? 'mousemove' : 'touchmove';
   wordsearch.removeEventListener(whichmove, whileDragging, false);
-  localStorage.setItem('saved', JSON.stringify(alltables));
+  localStorage.setItem('savedTables', JSON.stringify(allTables));
 }
+
 wordsearch.addEventListener('mousedown', startDrag, false);
-wordsearch.addEventListener('mouseup', endDrag, false);
+document.addEventListener('mouseup', endDrag, false);
 
 wordsearch.addEventListener('touchstart', startDrag, false);
-wordsearch.addEventListener('touchend', endDrag, false);
+document.addEventListener('touchend', endDrag, false);
 
 window.addEventListener('beforeunload', () => {
-  localStorage.setItem('saved', JSON.stringify(alltables));
+  localStorage.setItem('savedTables', JSON.stringify(allTables));
 }, false);
 
-//make a new table
-newtable.addEventListener('click', function() {
+// Make a new table
+newtable.addEventListener('click', () => {
   let confNew;
   if (!savedyet) {
     confNew = confirm('Make a new table without saving?');
   }
   if (confNew || savedyet) {
-    savetable.textContent = 'Save';
-    savetable.disabled = false;
+    saveTable.textContent = 'Save';
+    saveTable.disabled = false;
     backbtn.classList.add('none');
     savedName.textContent = '';
     document.title = 'Random Word Search';
     shown = null;
     sessionStorage.setItem('shown', JSON.stringify(shown));
 
-    savedyet ? savedyet = false : alltables.splice(alltables.length-1, 1);
+    savedyet ? savedyet = false : allTables.splice(allTables.length - 1, 1);
     localStorage.setItem('savedyet', savedyet);
-    localStorage.setItem('saved', JSON.stringify(alltables));
+    localStorage.setItem('savedTables', JSON.stringify(allTables));
     draw(true);
   }
 }, false);
 
-function saveTheTable() { //save a new table
+/** Save a new table to localStorage */
+function saveTheTable() {
   if (givename.value !== '' && !savedyet) {
-    alltables[alltables.length-1].name = givename.value;
+    allTables[allTables.length - 1].name = givename.value;
     tableName = givename.value;
     sessionStorage.setItem('tableName', JSON.stringify(tableName)); // save which table the user is viewing
-    shown = alltables.length - 1; //get the number of the table 
+    shown = allTables.length - 1; //get the number of the table 
     sessionStorage.setItem('shown', JSON.stringify(shown)); //save the number of the table
     ifSaved(); //change buttons for saved
-    localStorage.setItem('saved', JSON.stringify(alltables));
+    localStorage.setItem('savedTables', JSON.stringify(allTables));
     closeAll();
     savedyet = true;
     localStorage.setItem('savedyet', JSON.stringify(savedyet));
     givename.value = '';
     // draw(); //draw the new table
   }
-  savetable.textContent = 'Saved!';
-  savetable.disabled = true;
+  saveTable.textContent = 'Saved!';
+  saveTable.disabled = true;
 }
 
-//open the save table popup
-savetable.addEventListener('click', function() {
+// open the save table popup
+saveTable.addEventListener('click', () => {
   savepopup.style.display = 'inline-block';
   shadow.style.display = 'initial';
   givename.focus();
   savepop = true;
 }, false);
 
-//actually save the table
+// Actually save the table
 reallysave.addEventListener('click', saveTheTable, false);
 
-//open the list of saved tables, and add links to all of them
-saved.addEventListener('click', function() {
-  alltables = gotchem('saved', []);
-  for (let i = 0; i < alltables.length; i++) {
-    alltables[i].number = i+1;
+// open the list of saved tables, and add links to all of them
+saved.addEventListener('click', () => {
+  allTables = gotchem('savedTables', []);
+  for (let i = 0; i < allTables.length; i++) {
+    allTables[i].number = i + 1;
   }
   savedpopup.style.display = 'inline-block';
   shadow.style.display = 'initial';
   savedTables.innerHTML = '';
-  for (let i = alltables.length-1; i > -1; i--) {//minus so they end up in the right order
-    if (alltables[i].name !== '') {
-      let showname = document.createTextNode(alltables[i].number + '. ' + alltables[i].name);
+
+  for (let i = allTables.length - 1; i >= 0; i--) {// minus so they end up in the right order
+    if (allTables[i].name !== '') {
+      let showname = document.createTextNode(allTables[i].number + '. ' + allTables[i].name);
       let row = savedTables.insertRow(0);
       let cell0 = row.insertCell(0);
       let cell1 = row.insertCell(1);
@@ -311,27 +339,30 @@ savedTables.addEventListener('click', e => {
     draw(false, shown); //draw the new table
     closeAll(); //close the modal
   }
+
   else if (e.target.matches('.dltbtns')) {
     let confDelete = confirm(`Delete your saved word search "${tableName}"?`);
     if (confDelete) {
-      let getIndex = e.target.closest('tr').getElementsByTagName('td')[0].textContent.charAt(0)-1;
-      alltables.splice(getIndex, 1); //remove the right table
-      localStorage.setItem('saved', JSON.stringify(alltables)); //save that
-      if (alltables[shown-1] !== undefined) {
+      let getIndex = e.target.closest('tr').getElementsByTagName('td')[0].textContent.charAt(0) - 1;
+      allTables.splice(getIndex, 1); //remove the correct table
+      localStorage.setItem('savedTables', JSON.stringify(allTables)); //save that
+
+      // Try to find a saved table to show
+      if (allTables[shown - 1] !== undefined) {
         shown -= 1;
       }
-      else if (alltables[shown+1] !== undefined) {
+      else if (allTables[shown + 1] !== undefined) {
         shown += 1;
       }
       else {
         shown = null;
       }
       sessionStorage.setItem('shown', JSON.stringify(shown));
-      if (alltables.length === 0) {
-        savedyet = false; 
+      if (allTables.length === 0) {
+        savedyet = false;
         localStorage.setItem('savedyet', JSON.stringify(savedyet));
-        savetable.textContent = 'Save';
-        savetable.disabled = false;
+        saveTable.textContent = 'Save';
+        saveTable.disabled = false;
       }
       closeAll();
       draw(false, shown);
@@ -339,12 +370,12 @@ savedTables.addEventListener('click', e => {
   }
 }, false);
 
-//delete all tables
-deleteall.addEventListener('click', function() {
+// Delete all tables
+deleteall.addEventListener('click', () => {
   let rmvallconf = confirm('Remove all saved word searches?');
   if (rmvallconf) {
-    alltables.length = 0;
-    localStorage.setItem('saved', JSON.stringify(alltables));
+    allTables.length = 0;
+    localStorage.setItem('savedTables', JSON.stringify(allTables));
     savedyet = false;
     localStorage.setItem('savedyet', JSON.stringify(savedyet));
     backbtn.classList.add('none');
@@ -356,9 +387,13 @@ deleteall.addEventListener('click', function() {
   }
 }, false);
 
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode === 27) { closeAll(); }
-  if (e.keyCode === 13 && savepop) { saveTheTable(); }
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    closeAll();
+  }
+  else if (e.key === 'Enter' && savepop) {
+    saveTheTable();
+  }
 }, false);
 
 function closeAll() {
@@ -368,7 +403,7 @@ function closeAll() {
 };
 
 //switch l/d mode
-function switchMode(start=false) {
+function switchMode(start = false) {
   if (lightmode === start) {
     document.body.style.backgroundColor = 'white';
     wordsearch.style.color = 'black';
@@ -384,15 +419,15 @@ function switchMode(start=false) {
 }
 
 //light/dark mode
-ldmode.addEventListener('click', function()  {
+ldmode.addEventListener('click', function () {
   switchMode();
   lightmode = lightmode ? false : true;
   localStorage.setItem('mode', JSON.stringify(lightmode));
 }, false);
 
 //close modals on click outside
-document.addEventListener('mousedown', (evt) => { 
-  if(evt.target.closest('.popup')) return;
+document.addEventListener('mousedown', (evt) => {
+  if (evt.target.closest('.popup')) return;
   if (savepop || savedpop) { closeAll(); }
 }, false);
 
@@ -402,7 +437,7 @@ backbtn.addEventListener('click', () => {
   backbtn.classList.add('none');
   newtable.classList.remove('none');
   seeName.classList.add('none');
-  savetable.classList.remove('none');
+  saveTable.classList.remove('none');
   document.title = 'Random Word Search';
   shown = null;
   sessionStorage.setItem('shown', JSON.stringify(shown));
